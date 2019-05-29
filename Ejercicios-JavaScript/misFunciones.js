25d1afb4e0c936e2b0b80e940e2d0181f68cfa4f
santi2019 <santiagovietto5@hotmail.com>
2019-05-29 09:35:03 -0300

    hola

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,3 +9,34 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function suma(){
+    document.opeMat.sum_total.value=Number(document.opeMat.sum_num1.value) + Number(document.opeMat.sum_num2.value);
+}
+
+function resta(){
+    document.opeMat.res_total.value=Number( document.opeMat.res_num1.value) - Number(document.opeMat.res_num2.value);
+}
+function multiplicacion() {
+    document.opeMat.mul_total.value=Number( document.opeMat.mul_num1.value) * Number(document.opeMat.mul_num2.value);
+}
+function division() {
+    document.opeMat.div_total.value = Number(document.opeMat.div_num1.value) / Number(document.opeMat.div_num2.value);
+}
+function conv_Grados_Rad(id_elem, valor) {
+    if (id_elem == "grados") {
+        document.getElementById("radianes").value = valor * Math.PI / 180;
+    }
+    if (id_elem == "radianes") {
+        document.getElementById("grados").value = valor * 180 / Math.PI;
+    }
+}
+
+function mostrar_ocultar(id_elem){
+
+    if(id_elem=="ocultarDiv"){
+        document.getElementById("unDiv").style.display='none';
+    }
+    if(id_elem=="mostrarDiv"){
+        document.getElementById("unDiv").style.display='block';
+    }
+}
